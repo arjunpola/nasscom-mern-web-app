e021eb5c970ab1a97a22717555aae4cedbe0b84b
Nagarjun Pola <arjun.pola@gmail.com>
2024-07-09 12:54:41 +0530

    Create and display product cards

--- view/src/pages/HomePage.js
+++ view/src/pages/HomePage.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import ProductCard from "../components/ProductCard";
 
 const HomePage = () => {
+    const [productList, setProductList] = useState([]);
+
+    useEffect(() => {
+        getProduct();
+    }, []);
+
+    const getProduct = async () => {
+        try {
+            const response = await axios.get("/read");
+            setProductList(response.data);
+            console.log(response.data);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
     return (
         <React.Fragment>
-            <h2>My HomePage</h2>
+            {productList.length !== 0 &&
+                productList.map((product) => <ProductCard product={product} />)}
         </React.Fragment>
     );
 };
