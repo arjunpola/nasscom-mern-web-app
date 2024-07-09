9036267323668d8ee26da0b4e87bb9df1642c81c
Nagarjun Pola <arjun.pola@gmail.com>
2024-07-09 12:48:38 +0530

    Setup React Boilerplate

---
+++ view/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <React.Fragment>
+            <h2>My HomePage</h2>
+        </React.Fragment>
+    );
+};
+
+export default HomePage;
